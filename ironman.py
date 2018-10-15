8523a1ec26f397a5b30a7393a2a8f8e09f335a43
CorgigamingYT <43857110+CorgigamingYT@users.noreply.github.com>
2018-10-15 14:01:02 +0100

    ironman
    code part 3

--- ironman.py
+++ ironman.py
@@ -1,2 +1,14 @@
 #ironman game file.
+import time
+import os
 print("Hello, You have chosen to play as Ironman this particular time.")
+time.sleep(2)
+print("You walk into your lab and log on to your computer (top of the range)")
+time.sleep(2)
+print("You see that you have emails but you are tired.")
+time.sleep(2)
+print("""
+1. Look at them.
+2. Go to bed.
+3. Get a coffee.
+""")
