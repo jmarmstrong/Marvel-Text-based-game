9452cd845e45c5f11e83717a4be6c7d78a21b431
CorgigamingYT <43857110+CorgigamingYT@users.noreply.github.com>
2018-10-18 15:59:08 +0300

    ironman
    code part 5

--- ironman.py
+++ ironman.py
@@ -1,9 +1,10 @@
 #ironman game file.
 import time
 import os
+counter = 0
 print("Hello, You have chosen to play as Ironman this particular time.")
 time.sleep(2)
-print("You walk into your lab and log on to your computer (top of the range)")
+print("You walk into your lab and log on to your computer (top of the range).")
 time.sleep(2)
 print("You see that you have emails but you are tired.")
 time.sleep(2)
@@ -14,10 +15,35 @@ print("""
 """)
 choice = input("What do you do? ")
 if choice == "1":
-    #complete
-    print("hi")
+    counter += 1
+    print("You open the emails and you see an email from and unknown person.")
     time.sleep(1)
+    print("You open the email and it says you are already dead.")
+    time.sleep(1.5)
+    print("It explodes!")
+    time.sleep(1)
+    print("You die.")
+    time.sleep(2)
+    print("Please try again.")
+    time.sleep(2)
+    print("You survived" ,counter, "choices.")
+    time.sleep(3)
+    again = input("Do you want to play again? ")
+    if again == "yes":
+        time.sleep(2)
+        os.startfile("menu.py")
+    if again == "no":
+        time.sleep(3)
+        exit()
 if choice == "2":
-    #complete
+    counter += 1
+    print("hi")
 if choice == "3":
-    #complete
+    counter += 1
+    print("hi")
+if choice == "2":
+    counter += 1
+    print("hi")
+if choice == "3":
+    counter += 1
+    print("hi")
